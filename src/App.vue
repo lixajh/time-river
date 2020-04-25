2d126f841c0822d6fccde24fcd3e764944221dfe
xiaoli <lixajh@163.com>
2020-04-26 01:09:15 +0800

    展示时间轴

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,66 @@
 <template>
-  <div id="app">
+  <!-- <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div> -->
+
+  <div id="app">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+     <timeline  ref="timeline"
+    :items="timeline.items"
+    :groups="timeline.groups"
+    :options="timeline.options">
+    </timeline>
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+// import HelloWorld from './components/HelloWorld.vue'
+
+import { Timeline } from "vue2vis";
 
 export default {
+  
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Timeline
+  },
+
+  data: ()=>({
+    timeline: {
+      groups: [
+        {
+          id: 1,
+          content: "1111Group 1"
+        }
+      ],
+      items: [
+        {
+          id: 5,
+          group: 1,
+          content: "李晓出生",
+          start: "1992-09-21"
+        },
+        {
+          id: 6,
+          group: 1,
+          content: "嬴政出生",
+          start: new Date(-251, 7, 15)
+        
+        }
+      ],
+      options: {
+        editable: false
+      }
+    }
+  })
 }
 </script>
 
 <style>
+@import "../node_modules/vue2vis/dist/vue2vis.css";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
